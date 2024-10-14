b65b32b78907b6a6bef69483340175d74d100a21
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-10-14 12:45:44 +0800

    updates on dom manipulation

--- starter/script.js
+++ starter/script.js
@@ -4,3 +4,15 @@
 
 console.log(document.querySelector('.message').textContent);
 // selecting class and texts elements in HTML PAGE
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+
+// --
