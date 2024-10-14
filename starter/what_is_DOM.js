cdd15a5ad96db5b5af0f4441e6daf7665ba14eec
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-10-14 12:36:23 +0800

    added what is DOM lecture

---
+++ starter/what_is_DOM.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/*
+
+So what is the DOM actually?
+
+Well, DOM stands for Document Object Model, and it is,
+basically, a structured representation of HTML documents.
+The DOM allows us to use JavaScript to access HTML elements
+
+*/
